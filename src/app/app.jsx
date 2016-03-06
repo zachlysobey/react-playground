3b39e42a250fb6e791af07badeb19832fe9f16d1
zach lysobey <zach@lysobey.com>
2016-03-06 12:30:19 -0500

    convert App to 'pure' stateless component

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import SecondCounter from './second-counter.jsx';
 import FirebaseTest from './firebase-test.jsx';
 
-export default class extends React.Component {
-
-  render() {
-    const intervals = [1, 3, 5];
-    const counters = intervals.map(interval => (
-      <p key={interval}>
-        {interval} Second Counter:
-        {' '}
-        <SecondCounter secondInterval={interval} />
-      </p>
-    ));
-    return (
-      <div>
-        <h1>React Playground</h1>
-        {counters}
-        <FirebaseTest />
-      </div>
-    );
-  }
-
+export default function App() {
+  const intervals = [1, 3, 5];
+  const counters = intervals.map(interval => (
+    <p key={interval}>
+      {interval} Second Counter:
+      {' '}
+      <SecondCounter secondInterval={interval} />
+    </p>
+  ));
+  return (
+    <div>
+      <h1>React Playground</h1>
+      {counters}
+      <FirebaseTest />
+    </div>
+  );
 };
