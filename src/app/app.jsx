7be566ae4fc3f0d0d4de7c5fd1c39e76fd652323
zach lysobey <zach@lysobey.com>
2016-03-12 15:58:01 -0500

    use kanbanApp from kanban/index.jsx

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import SecondCounter from './second-counter.jsx';
 import FirebaseTest from './firebase-test.jsx';
-import cardList from './kanban/kanban-data.js';
-import KanbanBoard from './kanban/kanban-board.jsx';
+import KanbanApp from './kanban';
 
 export default () =>
   <div>
     <h1>React Playground</h1>
     <Counters intervals={[1, 3, 5]} />
     <FirebaseTest />
-    <KanbanBoard cards={cardList} />
+    <KanbanApp />
   </div>
 
 const Counters = ({intervals}) =>
